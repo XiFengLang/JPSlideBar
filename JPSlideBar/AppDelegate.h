f4bbf46fa5bb910bf7f75353541ee8d25c257311
apple <apple@appledeimac-3.airdream>
2015-12-30 10:55:18 +0800

    Initial Commit

---
+++ JPSlideBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JPSlideBar
+//
+//  Created by apple on 15/12/30.
+//  Copyright © 2015年 XiFengLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JPSlideBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JPSlideBar
+//
+//  Created by apple on 15/12/30.
+//  Copyright © 2015年 XiFengLang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
